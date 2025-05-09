913cc8fd3ab03a3113842f51e1a7ca03dd980f98
Fangjie Li <li.fangjie2001@gmail.com>
2025-05-08 19:07:40 -0500

    fixed random issue where users cannot see each other/are not in same room due to wrong region settings

--- Assets/MRTK.Tutorials.MultiUserCapabilities/Scripts/PhotonLobby.cs
+++ Assets/MRTK.Tutorials.MultiUserCapabilities/Scripts/PhotonLobby.cs
@@ -1,6 +1,7 @@
 ﻿using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MRTK.Tutorials.MultiUserCapabilities
 {
@@ -10,6 +11,11 @@ namespace MRTK.Tutorials.MultiUserCapabilities
 
         private int roomNumber = 1;
         private int userIdCount;
+        string roomName = "KidneyORRoom";
+        string lobbyName = "KidneyORLobby";
+        public Text debugText = null;
+        string currentLobbyName;
+
 
         private void Awake()
         {
@@ -31,6 +37,15 @@ namespace MRTK.Tutorials.MultiUserCapabilities
             GenericNetworkManager.OnReadyToStartNetwork += StartNetwork;
         }
 
+        private void CreateSpecificRoom()
+        {
+            TypedLobby customLobby = new TypedLobby(lobbyName, LobbyType.Default);
+
+            RoomOptions roomOptions = new RoomOptions();
+            roomOptions.MaxPlayers = 4;
+            PhotonNetwork.CreateRoom(roomName, roomOptions, customLobby);
+        }
+
         public override void OnConnectedToMaster()
         {
             var randomUserId = Random.Range(0, 999999);
@@ -39,7 +54,60 @@ namespace MRTK.Tutorials.MultiUserCapabilities
             PhotonNetwork.AuthValues.UserId = randomUserId.ToString();
             userIdCount++;
             PhotonNetwork.NickName = PhotonNetwork.AuthValues.UserId;
-            PhotonNetwork.JoinRandomRoom();
+
+            if (PhotonNetwork.CurrentLobby == null)
+            {
+                currentLobbyName = "Lobby is Null";
+                Debug.Log("Lobby is null");
+            }
+            else if (PhotonNetwork.CurrentLobby.Name == null)
+            {
+                currentLobbyName = "Lobby Name is Null";
+                Debug.Log("Lobby.Name is null");
+            } else
+            {
+                currentLobbyName = PhotonNetwork.CurrentLobby.Name;
+            }
+            //Debug.Log("Current Lobby: " + PhotonNetwork.CurrentLobby.Name);
+            //Debug.Log("Lobby Type: " + PhotonNetwork.CurrentLobby.Type);
+
+            //Debug.Log("Current Lobby: " + Lobby.name);
+            //Debug.Log("Lobby Type: " + Lobby.GetType());
+            //PhotonNetwork.JoinRandomRoom();
+
+            TypedLobby customLobby = new TypedLobby(lobbyName, LobbyType.Default);
+            PhotonNetwork.JoinLobby(customLobby);
+
+            //RoomOptions roomOptions = new RoomOptions();
+            //roomOptions.MaxPlayers = 4;
+            //PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+        }
+
+        public override void OnJoinedLobby()
+        {
+
+            if (PhotonNetwork.CurrentLobby == null)
+            {
+                currentLobbyName = "Lobby is Null";
+                Debug.Log("Lobby is null");
+            }
+            else if (PhotonNetwork.CurrentLobby.Name == null)
+            {
+                currentLobbyName = "Lobby Name is Null";
+                Debug.Log("Lobby.Name is null");
+            }
+            else
+            {
+                currentLobbyName = PhotonNetwork.CurrentLobby.Name;
+            }
+
+            currentLobbyName = PhotonNetwork.CurrentLobby.Name;
+            Debug.Log("Joined Lobby: " + currentLobbyName);
+
+            RoomOptions roomOptions = new RoomOptions();
+            roomOptions.MaxPlayers = 4;
+
+            PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, PhotonNetwork.CurrentLobby);
         }
 
         public override void OnJoinedRoom()
@@ -47,21 +115,31 @@ namespace MRTK.Tutorials.MultiUserCapabilities
             base.OnJoinedRoom();
 
             Debug.Log("\nPhotonLobby.OnJoinedRoom()");
+            string currentRegion = PhotonNetwork.CloudRegion;
+            Debug.Log("Connected to region: " + currentRegion);
             Debug.Log("Current room name: " + PhotonNetwork.CurrentRoom.Name);
             Debug.Log("Other players in room: " + PhotonNetwork.CountOfPlayersInRooms);
             Debug.Log("Total players in room: " + (PhotonNetwork.CountOfPlayersInRooms + 1));
+
+            if (debugText != null)
+            {
+                debugText.text = "Connected to region: " + currentRegion + "\nLobby Name: " + currentLobbyName +  "\nCurrent room name: " + PhotonNetwork.CurrentRoom.Name + "\n" + "Total players in room: " + (PhotonNetwork.CountOfPlayersInRooms + 1) ;
+            }
         }
 
         public override void OnJoinRandomFailed(short returnCode, string message)
         {
-            CreateRoom();
+            Debug.Log("Join failed: " + message);
+            //CreateRoom();
+            CreateSpecificRoom();
         }
 
         public override void OnCreateRoomFailed(short returnCode, string message)
         {
             Debug.Log("\nPhotonLobby.OnCreateRoomFailed()");
             Debug.LogError("Creating Room Failed");
-            CreateRoom();
+            //CreateRoom();
+            CreateSpecificRoom();
         }
 
         public override void OnCreatedRoom()
